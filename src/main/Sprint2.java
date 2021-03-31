36ed22256206814d17a843e5fa244fdac8dee9c4
KandasamyParthasarathy <kparthas@stevens.edu>
2021-03-31 16:59:24 -0400

    US 08, 16 code refactoring

--- src/main/Sprint2.java
+++ src/main/Sprint2.java
@@ -44,7 +44,7 @@ public class Sprint2 {
             String lastName = null;
             String[] formatName;
             formatName =  family.husbandName.split("/");
-            if(formatName.length > 2) {
+            if(formatName.length > 1) {
                 lastName = formatName[1].trim();
             }
 
