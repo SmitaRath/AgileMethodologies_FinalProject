24279850b537b4978bccf168bc400acadbf1d578
anirudh <sanirudh1998@gmail.com>
2021-04-03 19:13:27 -0400

    Merge branch 'development' into anirudh
    # Conflicts:
    #	src/GedcomReadParse.java

---
+++ src/main/Family.java
@@ -0,0 +1,74 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+
+public class Family{
+    public String id;
+    int idLineNo;
+    public String husbandId;
+    int husbandidLineNo;
+    String husbandName;
+    public String wifeId;
+    int wifeidLineNo;
+    String wifeName;
+    public String dateOfMarried;
+    int dateOfMarriedidLineNo;
+    Date marrriedDate;
+    public String dateOfDivided;
+    int dateOfDividedLineNo;
+    Date dividedDate;
+    ArrayList<String> child;
+    String divorced;
+
+    public Family() {
+        this.id = null;
+        this.husbandId = null;
+        this.wifeId = null;
+        this.husbandName = "NA";
+        this.wifeName = "NA";
+        this.child = new ArrayList<>();
+        this.divorced = "NA";
+        this.dateOfMarried = "NA";
+        this.dateOfDivided = "NA";
+    }
+
+    static int  extractInt(String s) {
+        String num = s.replaceAll("\\D", "");
+        // return 0 if no digits found
+        return num.isEmpty() ? 0 : Integer.parseInt(num);
+    }
+
+    /* Comparator for sorting the list by main.Family ID */
+    public static Comparator<Family> familyIdComparator = new Comparator<Family>() {
+       /* public int compare(Family s1, Family s2) {
+            String Id1 = s1.id.toUpperCase();
+            String Id2 = s2.id.toUpperCase();
+            //ascending order
+            return Id1.compareTo(Id2);
+        }*/
+
+        public int compare(Family s1, Family s2) {
+            String Id1 = s1.id.toUpperCase();
+            String Id2 = s2.id.toUpperCase();
+
+            return extractInt(Id1) - extractInt(Id2);
+        }
+    };
+
+    public String printChildren() {
+        String str = "NA";
+        for(int i = 0; i < this.child.size(); ++i) {
+            if(i == 0)
+                str = "{'" + this.child.get(i) + "'";
+            else
+                str += ", '" + this.child.get(i) + "'";
+
+            if(i == this.child.size()-1)
+                str += "}";
+        }
+        return str;
+    }
+
+}
---
+++ src/test/GedcomReadParseTest.java
@@ -0,0 +1,134 @@
+package test;
+
+import main.*;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GedcomReadParseTest {
+    Family f = new Family();
+    GedcomReadParse g1 = new GedcomReadParse();
+
+    /**
+     * us-01 test cases
+     * 1. date validation
+     * 2. date after current date
+     */
+    @Test
+    public void checkDate() throws Exception{
+        Date t1 = new Date("13 MAR 2021");
+        assertEquals(t1,g1.validateDate("13 MAR 2021"));
+        assertEquals(null,g1.validateDate("45 MAR 2021"));
+        assertEquals(null,g1.validateDate("MAR 13 2021"));
+        assertEquals(true,g1.validateDate(t1,"2021-03-13"));
+        t1=new Date("13 MAR 2022");
+        assertEquals(false,g1.validateDate(t1,"2022-03-13"));
+        assertEquals(true,g1.validateDate(t1,"NA"));
+
+    }
+
+    /**
+     * US-21
+     * validating gender
+     */
+
+    @Test
+    public void validateGender(){
+        Individual i1 = new Individual();
+        i1.id="I1";
+        i1.gender="F";
+        g1.individuals.add(i1);
+        assertEquals(false,g1.validateGenderForFamily("I1","M"));
+        assertEquals(true,g1.validateGenderForFamily("I1","F"));
+    }
+
+
+    /**
+     * US-07
+     * Difference between birth and death should be less than 150 years
+     */
+
+    @Test
+    public void differenceBetweenTwoAge() {
+        Date date1 = new Date("13 MAR 1850");
+        Date date2 = new Date("13 MAR 2020");
+        assertEquals(170, g1.differenceBetweenTwoAge(date1, date2));
+        assertNotEquals(150, g1.differenceBetweenTwoAge(date1, date2));
+    }
+
+    /**
+     * US-07
+     * Difference between birth and death should be less than 150 years
+     */
+
+    @Test
+    public void calculateDays() {
+        Date date1 = new Date();
+        assertEquals(0, g1.calculateDays(date1));
+        assertNotEquals(10, g1.calculateDays(date1));
+    }
+
+    @Test
+    /*Validating function ValidateBirthBeforeMarriage for us-02*/
+    public void ValidateUS02(){
+        Individual I1 = new Individual();
+        I1.id = "I10";
+        Individual I2 = new Individual();
+        I2.id="I11";
+        I1.dateOfBirth = "2020-05-13";
+        I2.dateOfBirth = "2003-08-20";
+        g1.individuals.add(I1);
+        g1.individuals.add(I2);
+        f.husbandId = "I10";
+        f.wifeId = "I11";
+        f.dateOfMarried = "2003-08-22";
+        g1.families.add(f);
+        assertEquals(true, g1.ValidateBirthBeforeMarriage(f.husbandId, f.dateOfMarried));
+        assertEquals(false, g1.ValidateBirthBeforeMarriage(f.wifeId, f.dateOfMarried));
+    }
+
+    @Test
+    /*Validate Date function used for us-02*/
+    public void correctDate(){
+        int year = 2001;
+        int month = 2;
+        int day = 29;
+        assertEquals(false,g1.validateDate(year,month,day));
+        assertEquals(false, g1.validateDate(2016,4,31));
+    }
+
+    @Test
+    //Validate Whether Id's are unique
+    public void ValidateUS22(){
+        Individual I1 = new Individual();
+        I1.id = "I10";
+        Individual I2 = new Individual();
+        I2.id = "I10";
+        Family f1 = new Family();
+        Family f2 = new Family();
+        f1.id = "F10";
+        f2.id = "F10";
+        g1.individuals.add(I1);
+        g1.individuals.add(I2);
+        g1.families.add(f1);
+        g1.families.add(f2);
+        g1.checkIndividualId();
+        g1.checkFamilyId();
+        assertEquals(true, g1.validateIdForIndividual(I1.id));
+        assertEquals(true, g1.validateIdForFamily(f1.id));
+    }
+
+    @Test
+    //Validating function BirthBeforeDeath for us03
+    public void ValidateUS03(){
+        Individual I1 = new Individual();
+        I1.id = "I10";
+        I1.dateOfBirth = "2017-04-27";
+        I1.death = "2016-09-27";
+        I1.alive = false;
+        g1.individuals.add(I1);
+        assertEquals(true, g1.ValidateBirthbeforeDeath(I1));
+    }
+}
